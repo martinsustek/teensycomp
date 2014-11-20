6f2388500f9c6dc1435dc67b9a3635329597c998
Martin Sustek <martin.sustek@seznam.cz>
2014-11-20 20:33:58 +0100

    Font, text printing

--- Video/Video.ino
+++ Video/Video.ino
@@ -2,10 +2,10 @@ const int ledPin = 13;
 
 IntervalTimer videoTimer;
 const int videoSyncPin = 14;
-const int videoRedPin = 15;
-const int videoGreenPin = 16;
+const int videoRedPin = 18;
+const int videoGreenPin = 19;
 const int videoBluePin = 17;
-const int videoIntensityPin = 18;
+const int videoIntensityPin = 16;
 
 uint16_t videoCurrentScanlineNumber = 0;
 
@@ -71,55 +71,87 @@ inline void videoDrawScanlineImage(uint8_t imageLine) {
   
   // picture
   uint32_t *pixelPtr;
-  pixelPtr = videoBuffer + (imageLine * 10);
-  for (uint16_t i = 0; i < /*320*/ 160; i += 8, pixelPtr++) {
-    uint32_t pixelValue = *pixelPtr;
-    
-//    PORTC = 0b00010000;//I
-//    PORTC = 0b00001000;//B
-//    PORTC = 0b00000100;//G
-//    PORTC = 0b00000010;//R
-    
-/*    digitalWriteFast(videoRedPin, (pixelValue >> 31) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 30) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 29) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 28) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 27) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 26) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 25) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 24) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 23) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 22) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 21) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 20) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 19) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 18) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 17) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 16) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 15) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 14) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 13) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 12) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 11) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 10) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 9) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 8) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 7) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 6) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 5) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 4) & 1);
-    //videoDelay(1); // 0.208us
-    digitalWriteFast(videoRedPin, (pixelValue >> 3) & 1);
-    digitalWriteFast(videoGreenPin, (pixelValue >> 2) & 1);
-    digitalWriteFast(videoBluePin, (pixelValue >> 1) & 1);
-    digitalWriteFast(videoIntensityPin, (pixelValue >> 0) & 1);
-    //videoDelay(1); // 0.208us*/
+  uint32_t *endPtr;
+  pixelPtr = videoBuffer + (imageLine * 40);
+  endPtr = pixelPtr + 40;
+  pixelPtr--;
+  while (++pixelPtr < endPtr) {
+    GPIOB_PDOR = (*pixelPtr) >> 28;
+    asm("NOP"); // 0.208us*/
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 24;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 20;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 16;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 12;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 8;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 4;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    GPIOB_PDOR = (*pixelPtr) >> 0;
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
+    asm("NOP");
   }
   
   // text-unsafe, front porch (end of line)
@@ -167,10 +199,175 @@ void videoClearScreen() {
 void videoTestPattern() {
   for (uint16_t i = 0; i < 8000; i++) {
     videoBuffer[i] = 0xE0000000;
-    if (((i / 10) % 8) == 0) {
+    if (((i / 40) % 8) == 0) {
       videoBuffer[i] = 0xEEEEEEEE;
     }
+    if (i > 4000) {
+      videoBuffer[i] = 0xE0E0E0E0;
+      if (((i / 40) % 2) == 0) {
+        videoBuffer[i] = 0x0E0E0E0E;
+      }
+    }
+    //videoBuffer[i] = 0xE0E0E0E0;
   }
+}
+
+
+unsigned char videoTextFont[][6] PROGMEM = {
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,// sp 
+  0x00, 0x00, 0x00, 0x2f, 0x00, 0x00,// ! 
+  0x00, 0x00, 0x07, 0x00, 0x07, 0x00,// " 
+  0x00, 0x14, 0x7f, 0x14, 0x7f, 0x14,// # 
+  0x00, 0x24, 0x2a, 0x7f, 0x2a, 0x12,// $ 
+  0x00, 0x62, 0x64, 0x08, 0x13, 0x23,// % 
+  0x00, 0x36, 0x49, 0x55, 0x22, 0x50,// & 
+  0x00, 0x00, 0x05, 0x03, 0x00, 0x00,// ' 
+  0x00, 0x00, 0x1c, 0x22, 0x41, 0x00,// ( 
+  0x00, 0x00, 0x41, 0x22, 0x1c, 0x00,// ) 
+  0x00, 0x14, 0x08, 0x3E, 0x08, 0x14,// * 
+  0x00, 0x08, 0x08, 0x3E, 0x08, 0x08,// + 
+  0x00, 0x00, 0x00, 0xA0, 0x60, 0x00,// , 
+  0x00, 0x08, 0x08, 0x08, 0x08, 0x08,// - 
+  0x00, 0x00, 0x60, 0x60, 0x00, 0x00,// . 
+  0x00, 0x20, 0x10, 0x08, 0x04, 0x02,// / 
+  0x00, 0x3E, 0x51, 0x49, 0x45, 0x3E,// 0 
+  0x00, 0x00, 0x42, 0x7F, 0x40, 0x00,// 1 
+  0x00, 0x42, 0x61, 0x51, 0x49, 0x46,// 2 
+  0x00, 0x21, 0x41, 0x45, 0x4B, 0x31,// 3 
+  0x00, 0x18, 0x14, 0x12, 0x7F, 0x10,// 4 
+  0x00, 0x27, 0x45, 0x45, 0x45, 0x39,// 5 
+  0x00, 0x3C, 0x4A, 0x49, 0x49, 0x30,// 6 
+  0x00, 0x01, 0x71, 0x09, 0x05, 0x03,// 7 
+  0x00, 0x36, 0x49, 0x49, 0x49, 0x36,// 8 
+  0x00, 0x06, 0x49, 0x49, 0x29, 0x1E,// 9 
+  0x00, 0x00, 0x36, 0x36, 0x00, 0x00,// : 
+  0x00, 0x00, 0x56, 0x36, 0x00, 0x00,// ; 
+  0x00, 0x08, 0x14, 0x22, 0x41, 0x00,// < 
+  0x00, 0x14, 0x14, 0x14, 0x14, 0x14,// = 
+  0x00, 0x00, 0x41, 0x22, 0x14, 0x08,// > 
+  0x00, 0x02, 0x01, 0x51, 0x09, 0x06,// ? 
+  0x00, 0x32, 0x49, 0x59, 0x51, 0x3E,// @ 
+  0x00, 0x7C, 0x12, 0x11, 0x12, 0x7C,// A 
+  0x00, 0x7F, 0x49, 0x49, 0x49, 0x36,// B 
+  0x00, 0x3E, 0x41, 0x41, 0x41, 0x22,// C 
+  0x00, 0x7F, 0x41, 0x41, 0x22, 0x1C,// D 
+  0x00, 0x7F, 0x49, 0x49, 0x49, 0x41,// E 
+  0x00, 0x7F, 0x09, 0x09, 0x09, 0x01,// F 
+  0x00, 0x3E, 0x41, 0x49, 0x49, 0x7A,// G 
+  0x00, 0x7F, 0x08, 0x08, 0x08, 0x7F,// H 
+  0x00, 0x00, 0x41, 0x7F, 0x41, 0x00,// I 
+  0x00, 0x20, 0x40, 0x41, 0x3F, 0x01,// J 
+  0x00, 0x7F, 0x08, 0x14, 0x22, 0x41,// K 
+  0x00, 0x7F, 0x40, 0x40, 0x40, 0x40,// L 
+  0x00, 0x7F, 0x02, 0x0C, 0x02, 0x7F,// M 
+  0x00, 0x7F, 0x04, 0x08, 0x10, 0x7F,// N 
+  0x00, 0x3E, 0x41, 0x41, 0x41, 0x3E,// O 
+  0x00, 0x7F, 0x09, 0x09, 0x09, 0x06,// P 
+  0x00, 0x3E, 0x41, 0x51, 0x21, 0x5E,// Q 
+  0x00, 0x7F, 0x09, 0x19, 0x29, 0x46,// R 
+  0x00, 0x46, 0x49, 0x49, 0x49, 0x31,// S 
+  0x00, 0x01, 0x01, 0x7F, 0x01, 0x01,// T 
+  0x00, 0x3F, 0x40, 0x40, 0x40, 0x3F,// U 
+  0x00, 0x1F, 0x20, 0x40, 0x20, 0x1F,// V 
+  0x00, 0x3F, 0x40, 0x38, 0x40, 0x3F,// W 
+  0x00, 0x63, 0x14, 0x08, 0x14, 0x63,// X 
+  0x00, 0x07, 0x08, 0x70, 0x08, 0x07,// Y 
+  0x00, 0x61, 0x51, 0x49, 0x45, 0x43,// Z 
+  0x00, 0x00, 0x7F, 0x41, 0x41, 0x00,// [ 
+  0x00, 0x55, 0x2A, 0x55, 0x2A, 0x55,// 55 
+  0x00, 0x00, 0x41, 0x41, 0x7F, 0x00,// ] 
+  0x00, 0x04, 0x02, 0x01, 0x02, 0x04,// ^ 
+  0x00, 0x40, 0x40, 0x40, 0x40, 0x40,// _ 
+  0x00, 0x00, 0x01, 0x02, 0x04, 0x00,// ' 
+  0x00, 0x20, 0x54, 0x54, 0x54, 0x78,// a 
+  0x00, 0x7F, 0x48, 0x44, 0x44, 0x38,// b 
+  0x00, 0x38, 0x44, 0x44, 0x44, 0x20,// c 
+  0x00, 0x38, 0x44, 0x44, 0x48, 0x7F,// d 
+  0x00, 0x38, 0x54, 0x54, 0x54, 0x18,// e 
+  0x00, 0x08, 0x7E, 0x09, 0x01, 0x02,// f 
+  0x00, 0x18, 0xA4, 0xA4, 0xA4, 0x7C,// g 
+  0x00, 0x7F, 0x08, 0x04, 0x04, 0x78,// h 
+  0x00, 0x00, 0x44, 0x7D, 0x40, 0x00,// i 
+  0x00, 0x40, 0x80, 0x84, 0x7D, 0x00,// j 
+  0x00, 0x7F, 0x10, 0x28, 0x44, 0x00,// k 
+  0x00, 0x00, 0x41, 0x7F, 0x40, 0x00,// l 
+  0x00, 0x7C, 0x04, 0x18, 0x04, 0x78,// m 
+  0x00, 0x7C, 0x08, 0x04, 0x04, 0x78,// n 
+  0x00, 0x38, 0x44, 0x44, 0x44, 0x38,// o 
+  0x00, 0xFC, 0x24, 0x24, 0x24, 0x18,// p 
+  0x00, 0x18, 0x24, 0x24, 0x18, 0xFC,// q 
+  0x00, 0x7C, 0x08, 0x04, 0x04, 0x08,// r 
+  0x00, 0x48, 0x54, 0x54, 0x54, 0x20,// s 
+  0x00, 0x04, 0x3F, 0x44, 0x40, 0x20,// t 
+  0x00, 0x3C, 0x40, 0x40, 0x20, 0x7C,// u 
+  0x00, 0x1C, 0x20, 0x40, 0x20, 0x1C,// v 
+  0x00, 0x3C, 0x40, 0x30, 0x40, 0x3C,// w 
+  0x00, 0x44, 0x28, 0x10, 0x28, 0x44,// x 
+  0x00, 0x1C, 0xA0, 0xA0, 0xA0, 0x7C,// y 
+  0x00, 0x44, 0x64, 0x54, 0x4C, 0x44,// z 
+  0x14, 0x14, 0x14, 0x14, 0x14, 0x14,// horiz lines 
+};
+
+uint16_t videoTextPosX = 0;
+uint8_t videoTextPosY = 0;
+uint8_t videoColorForeground = 0xE;
+uint8_t videoColorBackground = 0x0;
+
+void videoTextSetPos(uint16_t x, uint8_t y) {
+  videoTextPosX = x;
+  videoTextPosY = y;
+}
+
+void videoTextNextLine() {
+  videoTextPosX = 0;
+  videoTextPosY += 8;
+  if (videoTextPosY >= 200) {
+    videoTextPosY = 0; // TODO: scroll instead?
+  }
+}
+
+void videoTextNextChar() {
+  videoTextPosX += 6;
+  if (videoTextPosX >= 318) {
+    videoTextNextLine();
+  }
+}
+
+void videoSetPixel(uint16_t x, uint8_t y, uint8_t color) {
+  uint16_t address = 40 * y + x / 8;
+  uint8_t shift = (7 - (x % 8)) * 4;
+  videoBuffer[address] &= ~(((uint32_t)0xF) << shift);
+  videoBuffer[address] |= ((uint32_t)(color & 0xF)) << shift;
+}
+
+void videoTextDrawChar(const char ch) {
+  char id = ch - 32;
+  for (uint8_t column = 0; column < 6; column++) {
+    uint8_t data = pgm_read_byte(&(videoTextFont[id][column]));
+    for (uint8_t row = 0; row < 8; row++) {
+      uint8_t color = videoColorBackground;
+      if ((data >> row) & 1) {
+        color = videoColorForeground;
+      }
+      videoSetPixel(videoTextPosX + column, videoTextPosY + row, color);
+    }
+  }
+}
+
+void videoTextWriteChar(const char ch) {
+  if (ch == '\n') {
+    videoTextNextLine();
+  } else {
+    videoTextDrawChar(ch);
+    videoTextNextChar();
+  }
+}
+
+void videoTextWriteString(const char* ch) {
+  while (*ch != 0) {
+    videoTextWriteChar(*ch);
+    ch++;
+  }  
 }
 
 
@@ -187,15 +384,24 @@ void setup() {
   videoTimer.begin(videoDrawScanline, 64);
   
   videoTestPattern();
+  videoTextWriteString("Hello everybody!\nThis is amazing!");
 //  videoTimer.priority(0);
   
-  //Serial.begin(9600);
+//  Serial.begin(9600);
 }
 
 void loop() {
   digitalWriteFast(ledPin, HIGH);
   delay(50);
   digitalWriteFast(ledPin, LOW);
-  delay(950);
-  //Serial.printf("videoRedPin: %d\n", videoRedPin);
+  delay(9950);
+
+/*  for (uint16_t i = 0; i < 8000; i++) {
+    if ((i % 40) == 0) {
+      Serial.printf("\n");
+    }
+    Serial.printf("%8x", videoBuffer[i]);
+  }*/
+  
+  for(;;);
 }
